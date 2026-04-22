c719ca6031d8c77759e0efa244728c76a0c861d3
Evan Sharafuddin <evansharafuddin@wustl.edu>
2026-04-22 11:36:41 -0600

    worki

--- rlocusplots_chat.m
+++ rlocusplots_chat.m
@@ -7,7 +7,7 @@ syms mu beta_i beta_o K_i K_o
 syms alpha beta
 syms g_v g_T s
 
-F_NAIVE_GAIN = 0;
+F_NAIVE_GAIN = 1;
 
 A = [  0                      , 1                                    , 0                      , 0                              ;
       -(1+mu)*R_i^2*K_T / J_i , ( -(1+mu)*R_i^2*D_T - beta_i ) / J_i , (1+mu)*R_i^2*K_T / J_i , (1+mu)*R_i^2*D_T / J_i         ;
@@ -16,7 +16,7 @@ A = [  0                      , 1                                    , 0        
 
 K_v_i = g_v*J_i/R_i/K_i;
 K_v_o = g_v*J_o/R_o/K_o;
-K_T_tf = g_T * 1 / ( R_i*K_i/J_i + R_o*K_o/J_o );
+% K_T_tf = g_T * 1 / ( R_i*K_i/J_i + R_o*K_o/J_o );
 
 if F_NAIVE_GAIN
     B = [ 0                     , 0                     ;
@@ -24,10 +24,14 @@ if F_NAIVE_GAIN
           0                     , 0                     ;
           0                     , R_o*K_o / J_o * g_v ];
 else
-    B = [ 0                     , 0                     , 0        ;
-          R_i*K_i / J_i * K_v_i , 0                     , R_i*K_i / J_i * K_T_tf      ;
-          0                     , 0                     , 0       ;
-          0                     , R_o*K_o / J_o * K_v_o , R_o*K_o / J_o * K_T_tf         ];
+    B = [ 0                     , 0                 ;
+      R_i*K_i / J_i * K_v_i , 0                     ;
+      0                     , 0                     ;
+      0                     , R_o*K_o / J_o * K_v_o ];
+    % B = [ 0                     , 0                     , 0        ;
+    %       R_i*K_i / J_i * K_v_i , 0                     , R_i*K_i / J_i * K_T_tf      ;
+    %       0                     , 0                     , 0       ;
+    %       0                     , R_o*K_o / J_o * K_v_o , R_o*K_o / J_o * K_T_tf         ];
 end
 
 C = [ 0   , 1     , 0    , 0    ;
@@ -78,12 +82,12 @@ for f = 1:2
 
     Hs = subs(Hs_sym, symvec, symval);
     Hs = subs(Hs, g_v, 1);
-    Hs = subs(Hs, g_T, 1);
+    % Hs = subs(Hs, g_T, 1);
 
-    H_tf = tf(zeros(3));
+    H_tf = tf(zeros(2));
 
-    for i = 1:3
-        for j = 1:3
+    for i = 1:2
+        for j = 1:2
             [num, den] = numden(Hs(i,j));
             num = sym2poly(expand(num));
             den = sym2poly(expand(den));
@@ -108,33 +112,42 @@ for i = 1:2
         G2 = H_all{2}(i,j); % EOT
 
         % THIS is the correct multi-system call
-        h = rlocusplot(G1, G2);
+        % h = rlocusplot(G1, 'r');
+        h=rlocusplot(G1, 'r', G2, 'b');
 
         % styling
         setoptions(h, ...
-            'XLim', [-30 20], ...
+            'XLim', [-20 10], ...
             'YLim', [-75 75], ...
             'Grid','off')
 
         % manually set colors
-        ax = gca;
-        lines = findall(ax,'Type','Line');
-        set(lines(1:2:end),'Color','b') % BOT
-        set(lines(2:2:end),'Color','r') % EOT
+        % ax = gca;
+        % lines = findall(ax,'Type','Line');
+        % set(lines(1:end/2),'Color','b') % BOT
+        % set(lines(end/2:end),'Color','r') % EOT
 
         hold on
 
         % mark K = 40
-        p1 = rlocus(G1, 40);
-        p2 = rlocus(G2, 40);
+        if F_NAIVE_GAIN % at BOT, the feedback controller gain is approx 1/6 when consider the other terms
+            p1 = rlocus(G1, 40/6);
+            p2 = rlocus(G2, 40/6);
+    
+            p11 = rlocus(G1, 20/6);
+            p22 = rlocus(G2, 20/6);
+        else
+            p1 = rlocus(G1, 40);
+            p2 = rlocus(G2, 40);
+    
+            p11 = rlocus(G1, 20);
+            p22 = rlocus(G2, 20);
+        end
 
-        p11 = rlocus(G1, 20);
-        p22 = rlocus(G2, 20);
-
-        plot(real(p1), imag(p1), 'rx', 'MarkerSize',10,'LineWidth',2)
-        plot(real(p2), imag(p2), 'bx', 'MarkerSize',10,'LineWidth',2)
-        % plot(real(p11), imag(p11), 'rx', 'MarkerSize',10,'LineWidth',2)
-        % plot(real(p22), imag(p22), 'bx',  'MarkerSize',10,'LineWidth',2)
+        % plot(real(p1), imag(p1), 'rx', 'MarkerSize',10,'LineWidth',2)
+        % plot(real(p2), imag(p2), 'bx', 'MarkerSize',10,'LineWidth',2)
+        plot(real(p11), imag(p11), 'rx', 'MarkerSize',10,'LineWidth',2)
+        plot(real(p22), imag(p22), 'bx',  'MarkerSize',10,'LineWidth',2)
 
         title(sprintf('%s \\rightarrow %s', vara(j), varb(i)), ...
             'FontWeight','bold')
@@ -147,110 +160,6 @@ end
 
 legend({'BOT','EOT','BOT','EOT'})
 
-
-
-%%%%%%%%
-figure
-for i = 1
-    for j = 1:2
-        subplot(1,2,j)
-
-        G1 = H_all{1}(i,j); % BOT
-        G2 = H_all{2}(i,j); % EOT
-
-        % THIS is the correct multi-system call
-        h = rlocusplot(G1, G2);
-
-        % styling
-        setoptions(h, ...
-            'XLim', [-30 20], ...
-            'YLim', [-75 75], ...
-            'Grid','off')
-
-        % manually set colors
-        ax = gca;
-        lines = findall(ax,'Type','Line');
-        set(lines(1:2:end),'Color','b') % BOT
-        set(lines(2:2:end),'Color','r') % EOT
-
-        hold on
-
-        % mark K = 40
-        p1 = rlocus(G1, 40);
-        p2 = rlocus(G2, 40);
-
-        p11 = rlocus(G1, 20);
-        p22 = rlocus(G2, 20);
-
-        plot(real(p1), imag(p1), 'rx', 'MarkerSize',10,'LineWidth',2)
-        plot(real(p2), imag(p2), 'bx', 'MarkerSize',10,'LineWidth',2)
-        % plot(real(p11), imag(p11), 'rx', 'MarkerSize',10,'LineWidth',2)
-        % plot(real(p22), imag(p22), 'bx',  'MarkerSize',10,'LineWidth',2)
-
-        title(sprintf('%s \\rightarrow %s', vara(j), varb(i)), ...
-            'FontWeight','bold')
-
-        xlabel('Re','FontWeight','bold')
-        ylabel('Im','FontWeight','bold')
-        % set(gca,'FontWeight','bold')
-    end
-end
-
-legend({'BOT','EOT','BOT','EOT'})
-
-%%%
-
-figure 
-vara = ["u_i","u_o", "u_T"];
-varb = ["v_i","v_o", "T"];
-
-
-for i = 1:3
-    for j = 1:3
-        subplot(3,3,(i-1)*3 + j)
-
-        G1 = H_all{1}(i,j); % BOT
-        G2 = H_all{2}(i,j); % EOT
-
-        % THIS is the correct multi-system call
-        h = rlocusplot(G1, G2);
-
-        % styling
-        setoptions(h, ...
-            'XLim', [-30 20], ...
-            'YLim', [-75 75], ...
-            'Grid','off')
-
-        % manually set colors
-        ax = gca;
-        lines = findall(ax,'Type','Line');
-        set(lines(1:2:end),'Color','b') % BOT
-        set(lines(2:2:end),'Color','r') % EOT
-
-        hold on
-
-        % mark K = 40
-        p1 = rlocus(G1, 40);
-        p2 = rlocus(G2, 40);
-
-        p11 = rlocus(G1, 20);
-        p22 = rlocus(G2, 20);
-
-        plot(real(p1), imag(p1), 'rx', 'MarkerSize',10,'LineWidth',2)
-        plot(real(p2), imag(p2), 'bx', 'MarkerSize',10,'LineWidth',2)
-        % plot(real(p11), imag(p11), 'rx', 'MarkerSize',10,'LineWidth',2)
-        % plot(real(p22), imag(p22), 'bx',  'MarkerSize',10,'LineWidth',2)
-
-        title(sprintf('%s \\rightarrow %s', vara(j), varb(i)), ...
-            'FontWeight','bold')
-
-        xlabel('Re','FontWeight','bold')
-        ylabel('Im','FontWeight','bold')
-        % set(gca,'FontWeight','bold')
-    end
-end
-
-legend({'BOT','EOT','BOT','EOT'})
 
 
 % gv = 40; l = 0:20:L_tot;
